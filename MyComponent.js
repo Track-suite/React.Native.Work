db071f19dbc736d48c47e84d8443d643873f0f00
Track-suite <youngunner1998@gmail.com>
2021-11-15 01:41:04 -0800

    initial commit of React native project

---
+++ MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as ImagePicker from 'expo-image-picker'
+import { View, StyleSheet } from 'react-native';
+
+function MyComponent(props) {
+  return (
+    <View style={styles.container}></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default MyComponent;
